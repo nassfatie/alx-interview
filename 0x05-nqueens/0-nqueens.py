1fa32c4bbf934f2b25746b629a897a7ffaa379ef
nassfatie <nassfatie@gmail.com>
2023-08-03 23:35:38 +0300

    updated

--- 0x05-nqueens/0-nqueens.py
+++ 0x05-nqueens/0-nqueens.py
@@ -3,52 +3,25 @@
 import sys
 
 
-def nqueens(n, y, board):
+def nqueens(n: int):
     """
-    Method: nqueens - place n queens
-            on an n by n board so that
-            no queens are attacking any
-            others.
-    Parameters: n is an int that sets
-                board size and # of queens
-    Return: All possible solutions to
-            placement, in list of lists form
+    backtracking
     """
-    for x in range(n):
-        hold = 0
-        for q in board:
-            if x == q[1]:
-                hold = 1
-                break
-            if y - x == q[0] - q[1]:
-                hold = 1
-                break
-            if x - q[1] == q[0] - y:
-                hold = 1
-                break
-        if hold == 0:
-            board.append([y, x])
-            if y != n - 1:
-                nqueens(n, y + 1, board)
-            else:
-                print(board)
-            del board[-1]
+    matrix = [[0 for x in range(n)] for y in range(n)]
+    print(str(matrix))
 
 
-def main():
-    if len(sys.argv) != 2:
+if __name__ == "__main__":
+    if len(sys.argv) > 2 or len(sys.argv) < 2:
         print("Usage: nqueens N")
-        sys.exit(1)
-    try:
-        n = int(sys.argv[1])
-    except Exception:
-        print('N must be a number')
-        sys.exit(1)
-    if n < 4:
+        exit(1)
+
+    if not sys.argv[1].isdigit():
+        print("N must be a number")
+        exit(1)
+
+    if int(sys.argv[1]) < 4:
         print("N must be at least 4")
-        sys.exit(1)
+        exit(1)
 
-    nqueens(n, 0, [])
-
-if __name__ == '__main__':
-    main()
+    nqueens(int(sys.argv[1]))
