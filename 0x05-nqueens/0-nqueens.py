78fe04959f1665046af1f4deb3c39a8cc66484e1
nassfatie <nassfatie@gmail.com>
2023-08-03 23:40:57 +0300

    fixed

--- 0x05-nqueens/0-nqueens.py
+++ 0x05-nqueens/0-nqueens.py
@@ -1,27 +1,55 @@
 #!/usr/bin/python3
-"""Solving N Queens with Backtracing"""
-import sys
+""""
+0. N queens - task
+"""
+
+from sys import argv
 
 
-def nqueens(n: int):
+def is_NQueen(cell: list) -> bool:
+    """"
+    True if N Queen, False if not
     """
-    backtracking
+    row_number = len(cell) - 1
+    difference = 0
+    for index in range(0, row_number):
+        difference = cell[index] - cell[row_number]
+        if difference < 0:
+            difference *= -1
+        if difference == 0 or difference == row_number - index:
+            return False
+    return True
+
+
+def solve_NQeens(dimension: int, row: int, cell: list, outcome: list):
     """
-    matrix = [[0 for x in range(n)] for y in range(n)]
-    print(str(matrix))
+    Return result of N Queens recusrivley
+    """
+    # Base case
+    if row == dimension:
+        print(outcome)
+    else:
+        for col in range(0, dimension):
+            cell.append(col)
+            outcome.append([row, col])
+            if (is_NQueen(cell)):
+                solve_NQeens(dimension, row + 1, cell, outcome)
+            cell.pop()
+            outcome.pop()
 
 
-if __name__ == "__main__":
-    if len(sys.argv) > 2 or len(sys.argv) < 2:
-        print("Usage: nqueens N")
-        exit(1)
-
-    if not sys.argv[1].isdigit():
-        print("N must be a number")
-        exit(1)
-
-    if int(sys.argv[1]) < 4:
-        print("N must be at least 4")
-        exit(1)
-
-    nqueens(int(sys.argv[1]))
+if len(argv) != 2:
+    print('Usage: nqueens N')
+    exit(1)
+try:
+    N = int(argv[1])
+except BaseException:
+    print('N must be a number')
+    exit(1)
+if N < 4:
+    print('N must be at least 4')
+    exit(1)
+else:
+    outcome = []
+    cell = 0
+    solve_NQeens(int(N), cell, [], outcome)
